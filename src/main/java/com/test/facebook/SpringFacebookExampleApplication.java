cc3a1f25f5bc8c97317cb4481ef85b76e2c54d92
Oussama1342 <oussama.amari@sesame.com.tn>
2020-09-07 19:02:31 +0200

    first commit

---
+++ src/main/java/com/test/facebook/SpringFacebookExampleApplication.java
@@ -0,0 +1,14 @@
+package com.test.facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookExampleApplication.class, args);
+	}
+	
+
+}
